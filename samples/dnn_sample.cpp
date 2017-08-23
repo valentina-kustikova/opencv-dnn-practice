156504af4496bc8b0fa67da67dfb75e549dc0c20
valentina-kustikova <valentina.kustikova.gmail.com>
2017-08-23 13:22:43 +0300

    Work version

--- samples/dnn_sample.cpp
+++ samples/dnn_sample.cpp
@@ -14,11 +14,11 @@ using namespace cv::dnn;
 const char* kAbout = "Sample of DNN module usage.";
 
 const char* kOptions =
-    "{ @image         |  | image to process        }"
-    "{ @modelTxt      |  | prototxt                }"
-    "{ @modelBin      |  | Caffe model             }"
-    "{ @classes       |  | the list of class names }"
-    "{ h ? help usage |  | print help message      }";
+    "{ i  image         | <none> | image to process        }"
+    "{ mt modelTxt      | <none> | prototxt                }"
+    "{ mb modelBin      | <none> | Caffe model             }"
+    "{ c  classes       | <none> | the list of class names }"
+    "{ h ? help usage   |        | print help message      }";
 
 
 /* Find best class for the blob (i. e. class with maximal probability) */
@@ -32,7 +32,7 @@ static void getMaxClass(const Mat &probBlob, int *classId, double *classProb)
 }
 static std::vector<String> readClassNames(
     const char *filename = "synset_words.txt")
-{
+{   
     std::vector<String> classNames;
     std::ifstream fp(filename);
     if (!fp.is_open())
@@ -66,10 +66,25 @@ int main(int argc, char** argv)
         parser.printMessage();
         return 0;
     }
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return 0;
+    }
+
+    // Load image
+    String imgName(parser.get<String>("image"));
+    Mat img = imread(imgName);
+    if (img.empty())
+    {
+        std::cout << "Can't load image for classification:" << std::endl;
+        std::cout << "image: " << imgName << std::endl;
+        return 0;
+    }
 
     // Load model
-    String modelTxt(parser.get<string>(1));
-    String modelBin(parser.get<string>(2));
+    String modelTxt(parser.get<String>("modelTxt"));
+    String modelBin(parser.get<String>("modelBin"));
     Net net = dnn::readNetFromCaffe(modelTxt, modelBin);
     if (net.empty())
     {
@@ -81,17 +96,8 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    // Load image
-    string imgName = parser.get<string>(0);
-    Mat img = imread(imgName);
-    if (img.empty())
-    {
-        std::cout << "Can't load image for classification:" << std::endl;
-        std::cout << "image: " << imgName << std::endl;
-        return 0;
-    }
-
     // GoogLeNet accepts only 224x224 RGB-images
+    // Scalar(104, 117, 123) - mean value across full training set
     Mat inputBlob = blobFromImage(img, 1, Size(224, 224),
         Scalar(104, 117, 123)); //Convert Mat to batch of images
     Mat prob;
@@ -107,7 +113,7 @@ int main(int argc, char** argv)
     // Find the best class
     int classId;
     double classProb;
-    string classes = parser.get<string>(3);
+    string classes = parser.get<string>("classes");
     getMaxClass(prob, &classId, &classProb);
     std::vector<String> classNames = readClassNames(classes.c_str());
     std::cout << "Best class: #" << classId << " '" <<
